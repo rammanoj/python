f58a10c9e62862ecea7bf5d363215c5b42bc8ddd
Naman Shenoy <namanshenoy@users.noreply.github.com>
2017-10-18 03:04:32 -0400

    Subtraction in python

---
+++ python/subtract.py
@@ -0,0 +1,5 @@
+print('Python Subtractinator')
+num_1 = input('Enter the first number:\n')
+num_2 = input('Enter the second number:\n')
+
+print(str(num_1)+" - "+str(num_2) + " = " + str(num_1 - num_2))
